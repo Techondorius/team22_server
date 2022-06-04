094fdc8c49e6961a07826b50505606eafe43e4a4
Nameless <tanisyun0331@icloud.com>
2022-06-04 16:22:26 +0900

    微調整

--- api/main.py
+++ api/main.py
@@ -1,12 +1,13 @@
+from router import get_events, edit_events, utils
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
+from fastapi import FastAPI
 import sys
 sys.dont_write_bytecode = True
 
-from fastapi import FastAPI
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse
-from router import get_events, edit_events, utils
 
 app = FastAPI()
+
 
 @app.exception_handler(RequestValidationError)
 async def request_validation_handler(req, exc):
@@ -15,4 +16,3 @@ async def request_validation_handler(req, exc):
 app.include_router(get_events.router)
 app.include_router(edit_events.router)
 app.include_router(utils.router)
-
