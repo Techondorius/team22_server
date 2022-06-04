26858d393661f249886bf0b0de74ffa8c88eb408
Nameless <tanisyun0331@icloud.com>
2022-06-04 14:20:01 +0900

    イベント登録 API の実装

--- api/main.py
+++ api/main.py
@@ -1,7 +1,15 @@
+from pydantic import ValidationError
 from model import Event
 from fastapi import FastAPI
-from router import get_events
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from router import get_events, edit_events
 
 app = FastAPI()
 
+@app.exception_handler(RequestValidationError)
+async def request_validation_handler(req, exc):
+    return JSONResponse(status_code=400, content={"code": "InvalidParameter"})
+
 app.include_router(get_events.router)
+app.include_router(edit_events.router)
