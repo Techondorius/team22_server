63fbf06ba99aa310c45fa7c8b398fa42aa4d12ce
Nameless <tanisyun0331@icloud.com>
2022-06-05 15:14:48 +0900

    古いイベントを返さないように調整

--- api/router/get_events.py
+++ api/router/get_events.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from fastapi import APIRouter, Depends, HTTPException
 from model.event import Event
 from db import get_db
@@ -6,7 +7,7 @@ from sqlalchemy.orm import Session
 router = APIRouter()
 @router.get('/api/events')
 async def events_list(db: Session = Depends(get_db)):
-    events_list = db.query(Event).all()
+    events_list = db.query(Event).filter(Event.date > datetime.now()).all()
     output = []
     for i in events_list:
         model = i.toResultJSON()
@@ -22,7 +23,7 @@ async def event_detail(
     event_id: int,
     db: Session = Depends(get_db)
 ):
-    event_dict = db.query(Event).filter(Event.id == event_id).first()
+    event_dict = db.query(Event).filter(Event.id == event_id, Event.date > datetime.now()).first()
     if event_dict is None:
         raise HTTPException(status_code=404, detail="item_not_found")
     else:
