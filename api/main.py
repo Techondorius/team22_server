aa60724e0a9b32a583717beb7a42b45d2d2b4840
Nameless <tanisyun0331@icloud.com>
2022-06-05 11:31:40 +0900

    cors 対応（仮）

--- api/main.py
+++ api/main.py
@@ -2,11 +2,27 @@ from router import get_events, edit_events, utils
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import sys
 sys.dont_write_bytecode = True
 
 
 app = FastAPI()
+
+# FIXME: テスト環境用
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.exception_handler(RequestValidationError)
